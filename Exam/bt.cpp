e38248f6b496869bec341ca881de5642e701e90e
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2022-09-24 07:42:15 +0700

    d
    exam

--- Exam/bt.cpp
+++ Exam/bt.cpp
@@ -1,6 +1,25 @@
 #include<stdio.h>
-int main(){
+#include<math.h>
 
+float dientich(float a,float b,float c){
+	
+float p = (a+b+c)/ 2;
+return sqrt(p*(p - a)*(p - b)*(p - c));
+}
+
+int main(){
+float a, b, c;//nhap 3 canh tam giac
+
+printf("\n Nhap vao canh a:");scanf("%f",&a);
+printf("\n Nhap vao canh b:");scanf("%f",&b);
+printf("\n Nhap vao canh c:");scanf("%f",&c);
+
+if(a >= b+c|| b >= a+c || c>=a+b){
+	printf("Error! Du lieu khong hop le");
+}else{
+	printf("\n Du lieu hop le..");
+	printf("\n Dien tich tam giac la: %.2f",dientich(a,b,c));	
+}
 }
 
 
--- Exam/bt2.cpp
+++ Exam/bt2.cpp
@@ -1,6 +1,21 @@
 #include<stdio.h>
-int main(){
+#include<string.h>
 
+void upperCase(char s[100]) {
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            s[i] -= 32;
+        }
+    }
+    printf("%s\n", s);
+}
+
+int main() {
+    char s1[100];
+    scanf("%s", s1);
+    upperCase(s1);
+    return 0;
 }
 
 
